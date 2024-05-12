c377e79e0511cae34396cbb22842045f1b345c64
Selion11 <gdecaro@itba.edu.ar>
2024-05-12 15:21:24 -0300

    More updates on lambda some on bucket
    - Chequear modules para lambdas y dynamoDB
    - Hacer un test de que todo este andando

--- lambdas/main.tf
+++ lambdas/main.tf
@@ -4,14 +4,19 @@ module "lambda_function_creator"{
         {
             function_name = var.lambda_names[0]
             description = var.lambda_descriptions[0]
-            handler = "lambda1.handler"
-            source_path = "lambdas/lambda1"
+            handler = var.handler[0]
+            source_path = var.lambda_paths[0]
         },
         {
-            function_name = "lambda2"
-            description = "lambda2"
-            handler = "lambda2.handler"
-            source_path = "lambdas/lambda2"
+            function_name = var.lambda_names[1]
+            description = var.lambda_descriptions[1]
+            handler = var.handler[1]
+            source_path = var.lambda_paths[1]
+        },{
+            function_name = var.lambda_names[2]
+            description = var.lambda_descriptions[2]
+            handler = var.handler[2]
+            source_path = var.lambda_paths[2]
         }
     ]
 }
